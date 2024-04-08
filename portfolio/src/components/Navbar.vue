f0943602fdc1603a6b540ffa4732729fedcafaa1
4ndris1114 <10407531@ucn.dk>
2024-04-08 13:58:47 +0200

    some initial work

---
+++ portfolio/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex h-screen">
+        <nav class="bg-gray-800 text-white w-32 p-4 flex flex-col items-center" ref="navbar">
+            <ul class="space-y-20 pt-12">
+                <li>
+                    <homeicon />
+                </li>
+                <li>
+                    <personicon />
+                </li>
+                <li>
+                    <homeicon />
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { gsap } from 'gsap';
+import homeicon from './icons/homeicon.vue';
+import personicon from './icons/personicon.vue';
+
+const navbar = ref(null);
+
+onMounted(() => {
+    const tl = gsap.timeline();
+    tl.from(navbar.value, { duration: 2, x: -100, opacity: 0, ease: 'power3.out' });
+});
+</script>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-  <a class="ml-10 font-bold">testing testing</a>
+  <div class="flex">
+    <Navbar />
+    <div class="flex-1">
+      <div class="wrapperdiv">
+        <h1 class="text-2xl text-center pt-4">Welcome to My Portfolio</h1>
+        <div class="container mx-auto flex flex-wrap mt-10">
+          <div class="max-w-4xl p-6 bg-slate-200 rounded-lg">
+            <p class="text-gray-700 leading-relaxed text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Cras sit amet euismod sapien. Ut in libero molestie eros luctus fringilla. Donec tincidunt id augue sit
+              amet elementum. Proin sit amet nunc turpis. Quisque quis urna et augue porta pellentesque. Cras eget orci
+              non velit fermentum venenatis. Nullam varius vehicula elit et rhoncus.</p>
+          </div>
+          <div class="flex justify-end">
+            <img class="w-4/5 rounded-3xl shadow-md" src="@/assets/portrait.jpeg" alt="My Image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
